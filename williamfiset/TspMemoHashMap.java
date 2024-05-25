f6065cf77fa46e75def5b03bcaae66e15e0ee41e
unknown <wongyihern321@outlook.com>
2024-05-26 01:15:22 +0800

    restore memo hashmap

--- williamfiset/TspMemoHashMap.java
+++ williamfiset/TspMemoHashMap.java
@@ -1,6 +1,5 @@
 package williamfiset;
 
-
 import com.sun.management.OperatingSystemMXBean;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,41 +10,35 @@ import java.lang.*;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List; // Make sure to import the correct MXBean for CPU load
-// JustWriteDataBerlin
-public class JustWriteDataBerlin {
+import java.util.*;
+
+public class TspMemoHashMap {
 
     private final int N, start;
-    private final double[][] distanceMatrix;
+    private final double[][] distance;
     private List<Integer> tour = new ArrayList<>();
     private double minTourCost = Double.POSITIVE_INFINITY;
     private boolean ranSolver = false;
 
-    public JustWriteDataBerlin(double[][] coordinates) {
-        this(0, coordinates);
+    public TspMemoHashMap(double[][] distance) {
+        this(0, distance);
     }
 
-    public JustWriteDataBerlin(int start, double[][] coordinates) {
-        N = coordinates.length;
+    public TspMemoHashMap(int start, double[][] distance) {
+        N = distance.length;
 
         if (N <= 2) {
             throw new IllegalStateException("N <= 2 not yet supported.");
+        }
+        if (N != distance[0].length) {
+            throw new IllegalArgumentException("Matrix must be square (n x n)");
         }
         if (start < 0 || start >= N) {
             throw new IllegalArgumentException("Invalid start node.");
         }
 
-        // Create distance matrix based on pairwise distances between coordinates
-        distanceMatrix = new double[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                distanceMatrix[i][j] = distance(coordinates[i], coordinates[j]);
-            }
-        }
-
         this.start = start;
+        this.distance = distance;
     }
 
     // Returns the optimal tour for the traveling salesman problem.
@@ -64,22 +57,21 @@ public class JustWriteDataBerlin {
         return minTourCost;
     }
 
-    // Solves the traveling salesman problem and caches solution.
-    public void solve() {
+    private void solve() {
 
         if (ranSolver) {
             return;
         }
 
         final int END_STATE = (1 << N) - 1;
-        Double[][] memo = new Double[N][1 << N];
+        Map<Integer, Double> memo = new HashMap<>();
 
         // Add all outgoing edges from the starting node to memo table.
         for (int end = 0; end < N; end++) {
             if (end == start) {
                 continue;
             }
-            memo[end][(1 << start) | (1 << end)] = distanceMatrix[start][end];
+            memo.put((1 << start) | (1 << end) | (end << 16), distance[start][end]);
         }
 
         for (int r = 3; r <= N; r++) {
@@ -97,12 +89,12 @@ public class JustWriteDataBerlin {
                         if (end == start || end == next || notIn(end, subset)) {
                             continue;
                         }
-                        double newDistance = memo[end][subsetWithoutNext] + distanceMatrix[end][next];
+                        double newDistance = memo.getOrDefault((subsetWithoutNext | (end << 16)), Double.POSITIVE_INFINITY) + distance[end][next];
                         if (newDistance < minDist) {
                             minDist = newDistance;
                         }
                     }
-                    memo[next][subset] = minDist;
+                    memo.put((subset | (next << 16)), minDist);
                 }
             }
         }
@@ -112,17 +104,17 @@ public class JustWriteDataBerlin {
             if (i == start) {
                 continue;
             }
-            double tourCost = memo[i][END_STATE] + distanceMatrix[i][start];
+            double tourCost = memo.getOrDefault((END_STATE | (i << 16)), Double.POSITIVE_INFINITY) + distance[i][start];
             if (tourCost < minTourCost) {
                 minTourCost = tourCost;
             }
         }
 
+        // Reconstruct TSP path.
         int lastIndex = start;
         int state = END_STATE;
         tour.add(start);
 
-        // Reconstruct TSP path from memo table.
         for (int i = 1; i < N; i++) {
 
             int bestIndex = -1;
@@ -131,7 +123,7 @@ public class JustWriteDataBerlin {
                 if (j == start || notIn(j, state)) {
                     continue;
                 }
-                double newDist = memo[j][state] + distanceMatrix[j][lastIndex];
+                double newDist = memo.getOrDefault((state | (j << 16)), Double.POSITIVE_INFINITY) + distance[j][lastIndex];
                 if (newDist < bestDist) {
                     bestIndex = j;
                     bestDist = newDist;
@@ -144,26 +136,14 @@ public class JustWriteDataBerlin {
         }
 
         tour.add(start);
-        Collections.reverse(tour);
-
         ranSolver = true;
-    }
-
-    private double distance(double[] coord1, double[] coord2) {
-        // Euclidean distance formula
-        double xi = coord1[0];
-        double yi = coord1[1];
-        double xj = coord2[0];
-        double yj = coord2[1];
-        return Math.sqrt(Math.pow(xi - xj, 2) + Math.pow(yi - yj, 2));
     }
 
     private static boolean notIn(int elem, int subset) {
         return ((1 << elem) & subset) == 0;
     }
 
-    // This method generates all bit sets of size n where r bits
-    // are set to one. The result is returned as a list of integer masks.
+    // This method generates all bit sets of size n where r bits are set to one.
     public static List<Integer> combinations(int r, int n) {
         List<Integer> subsets = new ArrayList<>();
         combinations(0, 0, r, n, subsets);
@@ -171,36 +151,89 @@ public class JustWriteDataBerlin {
     }
 
     // To find all the combinations of size r we need to recurse until we have
-    // selected r elements (aka r = 0), otherwise if r != 0 then we still need to
-    // select
+    // selected r elements (i.e r = 0), otherwise if r != 0 then we still need to select
     // an element which is found after the position of our last selected element
     private static void combinations(int set, int at, int r, int n, List<Integer> subsets) {
-
-        // Return early if there are more elements left to select than what is
-        // available.
-        int elementsLeftToPick = n - at;
-        if (elementsLeftToPick < r) {
+        int elementsRemaining = n - at;
+        if (elementsRemaining < r) {
             return;
         }
 
-        // We selected 'r' elements so we found a valid subset!
         if (r == 0) {
             subsets.add(set);
         } else {
             for (int i = at; i < n; i++) {
-                // Try including this element
-                set ^= (1 << i);
-
+                set |= (1 << i);
                 combinations(set, i + 1, r - 1, n, subsets);
-
-                // Backtrack and try the instance where we did not include this element
-                set ^= (1 << i);
+                set &= ~(1 << i);
             }
         }
     }
 
+    // calc Euclidean Distance between two points
+    private static double calculateEuclidDistance(double[] point1, double[] point2) {
+        double dx = point1[0] - point2[0];
+        double dy = point1[1] - point2[1];
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    // Function to read data from file, for RocketFuel 
+    private static double[][] readCoordinatesFromFile(String cityFile, String coordinatesFile) {
+        // HashMap<Name of City, Coor of City>
+        HashMap<String, double[]> cityCoordinates = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(coordinatesFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.trim().split(",");
+
+                String cityName = parts[0].trim();
+                double latitude = Double.parseDouble(parts[1].trim());
+                double longitude = Double.parseDouble(parts[2].trim());
+                cityCoordinates.put(cityName, new double[]{latitude, longitude});
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Read from weights.intra file
+        try (BufferedReader br = new BufferedReader(new FileReader(cityFile))) {
+            int pointsSize = 20;
+            String line;
+            Set<String> uniqueCoordinates = new HashSet<>(); // Set to store unique coordinates
+            List<double[]> uniqueCoordinatesList = new ArrayList<>(); // List to store unique coordinates in order
+            while ((line = br.readLine()) != null && uniqueCoordinates.size() < pointsSize) {
+                String[] parts = line.trim().split("\\s+");
+                String city1 = parts[0].replaceAll("\\+", " ").split(",")[0].trim();
+                String city2 = parts[1].replaceAll("\\+", " ").split(",")[0].trim();
+
+                if (cityCoordinates.containsKey(city1) && cityCoordinates.containsKey(city2)) {
+                    double[] coord1 = cityCoordinates.get(city1);
+                    double[] coord2 = cityCoordinates.get(city2);
+                    String coordinateString1 = Arrays.toString(coord1);
+                    String coordinateString2 = Arrays.toString(coord2);
+                    if (!uniqueCoordinates.contains(coordinateString1)) {
+                        uniqueCoordinates.add(coordinateString1);
+                        uniqueCoordinatesList.add(coord1);
+                    }
+                    if (!uniqueCoordinates.contains(coordinateString2)) {
+                        uniqueCoordinates.add(coordinateString2);
+                        uniqueCoordinatesList.add(coord2);
+                    }
+                }
+            }
+            // Convert List to array
+            double[][] coordinates = new double[uniqueCoordinatesList.size()][2];
+            for (int i = 0; i < uniqueCoordinatesList.size(); i++) {
+                coordinates[i] = uniqueCoordinatesList.get(i);
+            }
+            return coordinates;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     // Reads coordinates from a .tsp file
-    public static double[][] readCoordinates(String filename, int numPoints) throws IOException {
+    public static double[][] readtsp(String filename, int numPoints) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filename));
         List<double[]> coordinatesList = new ArrayList<>();
         String line;
@@ -276,31 +309,59 @@ public class JustWriteDataBerlin {
     }
 
     public static void main(String[] args) {
-        try {
-           
-            int n = 10; // Choose up to 50 nodes
-            //String filename = "ISP data\\berlin52.tsp";
-            String filename = "ISP data\\ch71009.tsp";
-            double[][] coordinates = readCoordinates(filename, n);
-            coordinates = equirectangularProjection(coordinates);
+        // Measure start time
+        long startTime = System.currentTimeMillis();
 
-            String writefileName = "points10_china.txt";
-            //String writefileName = "points25_berlin.txt";
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(writefileName))) {
-                for (int i = 0; i < coordinates.length; i++) {
-                    writer.write(String.format("%.6f %.6f", coordinates[i][0], coordinates[i][1]));
-                    writer.newLine(); // Move to the next line after writing each coordinate pair
+        // Read coordinates from file
+        double[][] coordinates = readCoordinatesFromFile("ISP data\\weights.intra", "ISP data\\city_coordinates.txt");
+
+        // normalize
+        coordinates = equirectangularProjection(coordinates);
+        //coordinates = MDSProjection.mdsProjection(coordinates);
+
+        // Calculate the number of points
+        int n = coordinates.length;
+        System.out.printf("n is %d ", n);
+
+        // Calculate Euclidean distance matrix
+        double[][] distanceMatrix = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    distanceMatrix[i][j] = 0; // Distance from a point to itself is 0
+                } else {
+                    distanceMatrix[i][j] = calculateEuclidDistance(coordinates[i], coordinates[j]);
                 }
-                System.out.println("Coordinates have been written to " + writefileName);
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        }
+        int startNode = 0;
+        TspDynamicProgrammingIterative solver = new TspDynamicProgrammingIterative(startNode, distanceMatrix);
 
-          
+        System.out.println("Tour: " + solver.getTour());
+        System.out.println("Tour cost: " + solver.getTourCost());
 
+        // Write coordinates to a text file to pointsSize
+        String fileName = "points20_RocketFuel.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (int i = 0; i < coordinates.length; i++) {
+                writer.write(String.format("%.6f %.6f", coordinates[i][0], coordinates[i][1]));
+                writer.newLine(); // Move to the next line after writing each coordinate pair
+            }
+            System.out.println("Coordinates have been written to " + fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // Measure end time
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        // Print elapsed time
+        System.out.println("Elapsed time: " + elapsedTime + " milliseconds");
+
+        // Print final heap usage  // Print CPU usage
+        printHeapUsage();
+        printCpuUsage();
 
     }
 }
